2fd5d3391fc187acaa73c758fd5020297d1c6068
FlippyNat <100854483+FlippyNat@users.noreply.github.com>
2023-09-12 21:30:18 +0800

    added programme page and some linkage

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def facilities():
 def staffDirectory():
     return render_template("staffDirectory.html")
 
+@app.route('/programme')
+def programmes():
+    return render_template("programme.html")
+
 if __name__ == "__name__":
     app.run(debug=True)
 
